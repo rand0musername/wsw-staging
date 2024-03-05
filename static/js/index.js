4213c3ce9278e386ac4e60810a11f49ce9b0495b
Nikola Jovanovic <njov996@gmail.com>
2024-03-05 04:09:36 +0100

    Add examples partially

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,38 @@
-window.HELP_IMPROVE_VIDEOJS = false;
+$(function(){
+	fetch("static/texts/phd_response.html" /*, options */)
+    .then((response) => response.text())
+    .then((html) => {
+        document.getElementById("text-phd-response").innerHTML = html;
+    })
+    .catch((error) => {
+        console.warn(error);
+    });
+});
 
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
+window.addEventListener('load', function() {
+	function selectExample() {
+		this.parentElement.childNodes.forEach(e => !e.classList || e == this || e.classList.remove('active'));
+		document.getElementsByClassName("example-box")[0].childNodes.forEach(e => !e.classList || e.classList.remove('active'));
+		const example = document.querySelector(`.example-${this.classList[this.classList.length-1]}`)
+		this.classList.add('active');
+		example.classList.add('active');
+		updateDetector.call(document.querySelector("#detectorMode"));
+	}
+	document.querySelectorAll("button").forEach(e => e.addEventListener('click', selectExample));
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
-    }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
-    bulmaSlider.attach();
-
-})
+	function updateDetector() {
+		bodies=document.querySelectorAll(".example.active")[0].querySelectorAll(".chat-body");
+		detectors=document.querySelectorAll(".example.active")[0].querySelectorAll(".chat-detector");
+		if(this.checked) {
+		  // Checkbox is checked
+		  bodies.forEach(b => b.childNodes.forEach(e => !e.classList || e.classList.add('active')));
+		  detectors.forEach(e => !e.classList || e.classList.add('active'));
+		} else {
+		  // Checkbox is not checked
+		  bodies.forEach(b => b.childNodes.forEach(e => !e.classList ||  e.classList.remove('active')));
+		  detectors.forEach(e => !e.classList || e.classList.remove('active'));
+		}
+	}
+	this.document.getElementById('detectorMode').addEventListener('change', updateDetector);
+});
